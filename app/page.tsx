a71787f3d993ff0b2b486d2f25e70a5805812949
sharull9 <sharullmusicplaylist@gmail.com>
2023-06-11 20:35:17 +0530

    changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
-async function getPosts() {
-  const res = await fetch(`/api/post`, { cache: "no-store" });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+// async function getPosts() {
+//   const res = await fetch(`http://localhost:3000/api/post`, {
+//     cache: "no-store",
+//   });
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
 
 type post = {
   id: number;
@@ -15,8 +19,16 @@ type post = {
   published: boolean;
 };
 
-export default async function Home() {
-  const data = await getPosts();
+export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/post`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    console.log(data);
+  }, []);
+  // const data = await getPosts();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {data.map((post: post) => {
